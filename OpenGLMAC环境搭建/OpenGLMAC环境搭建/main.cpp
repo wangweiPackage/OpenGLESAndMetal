dfb1c0381ecf1a8f4926bc34fd95d083c1159620
wei wang <wangwei780@126.com>
2020-10-18 20:27:03 +0800

    搭建OpenGLDemo

---
+++ OpenGLMAC环境搭建/OpenGLMAC环境搭建/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLMAC环境搭建
+//
+//  Created by wei wang on 2020/10/18.
+//  Copyright © 2020 wei wang. All rights reserved.
+//
+
+#include <stdio.h>
+#include "GLTools.h"
+#include <glut/glut.h>
+int main(int argc,char* argv[])
+{
+    return 0;
+}
